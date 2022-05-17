b11114b4d8ccbe26b46361af0fc24b0419c922a2
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-05-18 01:30:52 +0530

    Added local plugin window to front

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'github_oauth_credentials.dart';
 import 'src/github_login.dart';
+import 'package:github/github.dart';
+import 'package:window_to_front/window_to_front.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,20 +35,32 @@ class MyHomePage extends StatelessWidget {
       githubClientSecret: githubClientSecret,
       githubScopes: githubScopes,
       builder: (context, httpClient) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(title),
-          ),
-          body: const Center(
-            child: Text(
-              'You are logged in to GitHub!',
-            ),
-          ),
-        );
+        WindowToFront.activate();
+        return FutureBuilder<CurrentUser>(
+            future: viewerDetail(httpClient.credentials.accessToken),
+            builder: (context, snapshot) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text(title),
+                ),
+                body:  Center(
+                  child: Text(
+                    snapshot.hasData ?
+                        'Hello, ${snapshot.data!.login}' :
+                        'Retrieving viewer login details ... '
+                  ),
+                ),
+              );
+            });
       },
       /*githubClientId: githubClientId,
       githubClientSecret: githubClientSecret,
       githubScopes: githubScopes,*/
     );
   }
+}
+
+Future<CurrentUser> viewerDetail(accessToken) {
+  final gitHub = GitHub(auth: Authentication.withToken(accessToken));
+  return gitHub.users.getCurrentUser();
 }
